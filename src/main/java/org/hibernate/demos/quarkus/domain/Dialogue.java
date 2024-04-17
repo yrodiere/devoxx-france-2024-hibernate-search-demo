c247c82b2428f4a792dd586b0d9ad33c9cc1d293
Katia Aresti <karesti@redhat.com>
2024-04-17 11:55:36 +0200

    Add imports to be ready for the change

--- src/main/java/org/hibernate/demos/quarkus/domain/Dialogue.java
+++ src/main/java/org/hibernate/demos/quarkus/domain/Dialogue.java
@@ -4,9 +4,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 import org.hibernate.Length;
+import org.hibernate.demos.quarkus.ai.EmbeddingModelBridge;
+import org.hibernate.search.engine.backend.types.VectorSimilarity;
+import org.hibernate.search.mapper.pojo.bridge.mapping.annotation.ValueBridgeRef;
 import org.hibernate.search.mapper.pojo.mapping.definition.annotation.FullTextField;
 import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
 import org.hibernate.search.mapper.pojo.mapping.definition.annotation.IndexedEmbedded;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.VectorField;
 
 
 @Entity(name = "dialogue")
